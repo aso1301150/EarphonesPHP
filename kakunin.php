fa4f5466d530c9b5265f05529735f8278b3c4ae0
student <student@PC634S10.asost.local>
2015-06-16 10:36:18 +0900

    6/16高木コミット

---
+++ kakunin.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="ja" xml:lang="ja">
+<head>
+<meta charset="UTF-8">
+	<meta http-equiv="Content-Type" content="text/html;charset=Shift_JIS" />
+	<meta http-equiv="Content-Style-Type" content="text/css" />
+	<link rel="stylesheet" href="css.css" type="text/css" />
+<title>Earphones</title>
+</head>
+<body>
+
+<!--ヘッダーここから-->
+<a name="top" id="top"></a>
+<h1>ホームページテンプレート ECサイト、ネットショップ向け #017_green_3（見出しタグ&lt;h1&gt;を使用）</h1>
+<div id="header">
+<!--タイトルロゴここから-->
+<div id="header_left2">I ❤ Earphones</div>
+<!--画像にしたい場合は上記の<div id="header_left2">を<div id="header_left">に変更して画像を入れてください-->
+<!--タイトルロゴここまで-->
+<div id="header_right">
+<ul>
+<li><a href="#">サイトマップ</a></li>
+<li><a href="#">お問い合わせ</a></li>
+</ul>
+</div>
+<div style="clear:both;"></div>
+</div>
+<!--ヘッダーここまで-->
+
+<!--ヘッダーメニューここから-->
+<div id="menu">
+<ul>
+<li><a href="top.html">トップページ</a></li>
+<li><a href="#2">会員登録</a></li>
+<li><a href="#3">商品検索</a></li>
+<li><a href="#4">comingsoon...</a></li>
+<li><a href="#5">会員情報</a></li>
+</ul>
+</div>
+<div style="clear:both;"></div><!--デザインが崩れるので削除しない事-->
+<!--ヘッダーメニューここまで-->
+
+<div id="container"><!--"container"を削除するとデザインが崩れます-->
+
+<div id="main2"><!--デザインが崩れるので削除しない事-->
+
+<!--メインここから-->
+<div id="main">
+
+<!--パンくずリストここから-->
+<div class="pan"><a href="top.html">トップ</a> ＞ <a href="login.html">ログイン</a> </div>
+<!--パンくずリストここまで-->
+
+<!-- 新規会員登録確認 -->
+<div class="category">
+<h2 class="h2_normal">新規会員登録:確認</h2>
+<div class="login_body">
+<?php print "お名前".$_POST["name"]."<br><br>" ?>
+<?php print "メールアドレス".$_POST["meado"]."<br><br>" ?>
+<?php print "電話番号".$_POST["tell1"]."-".$_POST["tell2"]."-".$_POST["tell3"]."<br><br>" ?>
+<?php print "郵便番号".$_POST["tokoro"]."-".$_POST["tokoro2"]."<br><br>" ?>
+<?php print "住所".$_POST["prefecture"].$_POST["address"]."<br><br>" ?>
+
+</div>
+</div>
+<!-- 新規会員登録確認ここまで -->
+
+
+<div class="modoru"><a href="#top">ページ上に戻る</a></div>
+
+</div>
+<!--メインここまで-->
+
+<!--[if !IE]>サイドメニューここから<![endif]-->
+<div id="sub">
+
+<div class="category">
+<h3>アカウント</h3>
+<div class="entry_body">
+<div class="sub-btn"><a href="#">ログイン</a></div>
+<div class="sub-btn"><a href="#">はじめてのお客様</a></div>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div class="category">
+<h3>お買い物かご</h3>
+<div class="entry_body">
+<div class="sub-btn"><a href="#">カートの中を見る</a></div>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div class="category">
+<h3>お支払い方法</h3>
+<div class="entry_body">
+<p>お支払いは、カード決済、銀行振込、代金引換からお選びいただけます。</p>
+<p align="right"><a href="#">くわしくはこちら</a></p>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div class="category">
+<h3>送料について</h3>
+<div class="entry_body">
+<p>発送は平中運輸から、<br />
+  送料は全国一律500円になります。</p>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div align="center"><img src="images/188.gif" width="188" height="100" />
+
+</div>
+<div class="soto">
+<p>&lt;div class=&quot;category&quot;&gt;と<br />
+  &lt;div class=&quot;entry_body&quot;&gt;の外にテキストを入れたい時には&lt;div class=&quot;soto&quot;&gt;で囲んでください</p>
+</div>
+
+</div>
+
+<!--[if !IE]>サイドメニューここまで<![endif]-->
+
+<div style="clear:both;"></div><!--デザインが崩れるので削除しない事-->
+
+</div><!--"main2"-->
+
+<!--サイドメニュー2ここから-->
+<div id="sub2">
+<div class="category">
+<h3>Brand</h3>
+<div class="entry_body">
+<ul>
+<li><a href="#4">Audio-technica</a></li>
+<li><a href="#4">SONY</a></li>
+<li><a href="#4">Panasonic</a></li>
+<li><a href="#4">ELECOM</a></li>
+<li><a href="#4">Pioneer</a></li>
+</ul>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div class="category">
+<h3>機能・特徴</h3>
+<div class="entry_body">
+<ul>
+<li><a href="#4">インナーイヤー</a></li>
+<li><a href="#4">カナル型</a></li>
+<li><a href="#4">Bluetooth</a></li>
+<li><a href="#4">スポーツ</a></li>
+<li><a href="#4">ノイズキャンセル</a></li>
+</ul>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div class="category">
+<h3>coler</h3>
+<div class="entry_body">
+<ul>
+<li><a href="#4">red</a></li>
+<li><a href="#4">blue</a></li>
+<li><a href="#4">green</a></li>
+<li><a href="#4">black</a></li>
+<li><a href="#4">white</a></li>
+<li><a href="#4">other</a></li>
+</ul>
+</div>
+<div class="sub_bottom"></div>
+</div>
+
+<div align="center"><img src="images/188.gif" width="188" height="100" />
+
+</div>
+<div class="soto">
+<p>&lt;div class=&quot;category&quot;&gt;と<br />
+  &lt;div class=&quot;entry_body&quot;&gt;の外にテキストを入れたい時には&lt;div class=&quot;soto&quot;&gt;で囲んでください</p>
+</div>
+</div>
+<!--サイドメニュー2ここまで-->
+
+</div><!--"container"-->
+
+<!--フッターここから-->
+<div id="footer">
+<p><a href="#">運営会社</a>｜<a href="#">プライバシーポリシー</a>｜<a href="#">特定商取引法に基づく表記</a></p>
+<p>Copyright(C) ホームページ名 All Rights Reserved.</p>
+<!--ここから記述の変更、削除を禁止します。-->
+<p>Template design by <a href="http://homuten.com">homuten</a></p>
+<!--ここまで記述の変更、削除を禁止します。-->
+</div>
+<!--フッターここまで-->
+
+
+
+</body>
+</html>
